905e9fd1d5760d2048c21f73f06373eeb9c8ccfd
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-05-22 15:14:23 +0000

    Initial config
    Some parts are ported over from my NixOS configuration.

---
+++ nix-on-droid.nix
@@ -0,0 +1,49 @@
+{ config, lib, pkgs, ... }:
+
+{
+  android-integration.xdg-open.enable = true;
+
+  # Simply install just the packages
+  environment.packages = with pkgs; [
+    # User-facing stuff that you really really want to have
+    nano # or some other editor, e.g. nano or neovim
+    helix
+    git
+    gh
+
+    # Some common stuff that people expect to have
+    #procps
+    #killall
+    #diffutils
+    #findutils
+    #utillinux
+    #tzdata
+    #hostname
+    #man
+    #gnugrep
+    #gnupg
+    #gnused
+    #gnutar
+    #bzip2
+    #gzip
+    #xz
+    #zip
+    #unzip
+  ];
+
+  # Backup etc files instead of failing to activate generation if a file already exists in /etc
+  environment.etcBackupExtension = ".bak";
+
+  home-manager.config = ./home.nix;
+
+  # Read the changelog before changing this value
+  system.stateVersion = "24.05";
+
+  # Set up nix for flakes
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  # Set your time zone
+  #time.timeZone = "Europe/Berlin";
+}
